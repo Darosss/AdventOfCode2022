7ddb10b557ab663e3a8bf01660e87f4337235be8
Darosss <Daroing12@gmail.com>
2022-12-08 19:04:56 +0100

    refactor: move function from 3.2 to 3 and delete 3.2

--- 3/3.js
+++ 3/3.js
@@ -19,6 +19,24 @@ function getRepeatedItem(first, second) {
   return repeated;
 }
 
+function getRepeatedItemInGroupOf3(first, second, third) {
+  let foundRepated = false;
+  let i = 0;
+  let repeated = null;
+  while (!foundRepated || i >= first.length) {
+    if (
+      second.find((x) => x === first[i]) &&
+      third.find((x) => x === first[i])
+    ) {
+      foundRepated = true;
+      repeated = first[i].charCodeAt(0);
+    } else {
+      i++;
+    }
+  }
+  return repeated;
+}
+
 function calculateItemPriority(charCode) {
   let sum = 0;
   if (charCode >= 97) {
@@ -35,6 +53,7 @@ function getSumOfPriorities(fileName) {
   const fileLen = file.length;
 
   let sumOfProperties = 0;
+  let sumOfPropertiesGroups = 0;
 
   for (let i = 0; i < fileLen; i++) {
     let itemsLen = file[i].length - 1;
@@ -43,10 +62,19 @@ function getSumOfPriorities(fileName) {
     let secondComp = file[i].slice(itemsLenHalf).split("");
 
     let itemCharCode = getRepeatedItem(firstComp, secondComp);
-
     sumOfProperties += calculateItemPriority(itemCharCode);
+
+    if (i % 3 === 0) {
+      let itemGroupOf3Code = getRepeatedItemInGroupOf3(
+        file[i].split(""),
+        file[i + 1].split(""),
+        file[i + 2].split("")
+      );
+      sumOfPropertiesGroups += calculateItemPriority(itemGroupOf3Code);
+    }
   }
-  return sumOfProperties;
+  console.log("3. Part1: ", sumOfProperties);
+  console.log("3. Part2: ", sumOfPropertiesGroups);
 }
 
-console.log(getSumOfPriorities("data.txt"));
+getSumOfPriorities("data.txt");
